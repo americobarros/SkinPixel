dae5ffe2865e9052b9b695710ca57d5dba505e63
Hannah Brooks <hannah.brooks@mail.utoronto.ca>
2021-03-05 19:21:47 -0500

    made cards have info

--- frontend/src/components/SkinCard.js
+++ frontend/src/components/SkinCard.js
@@ -1,8 +1,22 @@
 import './SkinCard.css';
 
-export default function SkinCard() {
+export default function SkinCard(props) {
+    const { skin } = props;
+
     return (
-        <div className="CardStyle" />
+        <div className="CardStyle">
+            <div className="cardContainer">
+                <img src={skin.image} alt={skin.name} className="skinImage" />
+            </div>
+            <div className="cardInfo">
+                <div className="skinName">
+                    {skin.name}
+                </div>
+                <div>
+                    {skin.user.name}
+                </div>
+            </div>
+        </div>
     );
 }
   
