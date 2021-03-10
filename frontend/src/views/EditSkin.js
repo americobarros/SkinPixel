17e9d79e59c27fea3b6338c3dddde1fc2a90959a
Hannah Brooks <hannah.brooks@mail.utoronto.ca>
2021-03-09 21:19:17 -0500

    added color picker

--- frontend/src/views/EditSkin.js
+++ frontend/src/views/EditSkin.js
@@ -1,16 +1,34 @@
+import { useState } from 'react';
 import { Link, useParams } from "react-router-dom";
+import { SwatchesPicker } from 'react-color';
 
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import Button from '@material-ui/core/Button';
 
 import './EditSkin.css';
 
-export default function ViewSkin() {
+export default function ViewSkin(props) {
+  const { allSkins } = props;
   let { skinId } = useParams();
+
+  const skin = allSkins.find(skin => skin.id == skinId);
+
+  const [color, setColor] = useState(null);
+  const [rerender, setRerender] = useState(false);
+
+  function handleChangeComplete(color, event) {
+    setColor(color.hex.toString());
+  };
+
+  function setSkinColor(outer_index, inner_index) {
+    if (color) {
+      skin.skin2D[outer_index][inner_index] = color;
+    }
+    setRerender(!rerender);
+  }
 
   return (
     <div style={{ maxWidth: '1400px' }}>
-      {console.log('hi')}
       <div style={{ display: 'flex' }}>
         <Link to="/" style={{ color: 'black', alignSelf: 'center' }}>
           <ArrowBackIcon className="backIcon" />
@@ -19,9 +37,19 @@ export default function ViewSkin() {
         <h2 style={{ display: 'none' }}>{skinId}</h2>
       </div>
       <div style={{ display: 'flex' }}>
-        <div id="threeDView" />
+        <div id="threeDView">
+          {skin.skin2D.map((row, outer_idx) =>
+            <div style={{ display: 'flex' }}>
+              {row.map((color, inner_idx) => (
+                <div>
+                  <div className="cube" onClick={() => setSkinColor(outer_idx, inner_idx)} style={{ backgroundColor: color }} />
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
         <div className="rightItems">
-          <div id="colorPicker" />
+            <SwatchesPicker height="600px" onChangeComplete={handleChangeComplete}/>
           <Button className="ButtonStyle" variant="outlined">Save</Button>
           <Button className="ButtonStyle" variant="outlined">Delete</Button>
         </div>
